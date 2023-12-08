c50237e17eb0e7134554eb1ff347036007dbfe70
mykytashamota <nik.shamota@gmail.com>
2023-12-08 11:21:49 +0200

    add mongoose

--- app.js
+++ app.js
@@ -1,38 +1,22 @@
-// import express from "express";
-// import logger from "morgan";
-// import cors from "cors";
-
-// import contactsRouter from "./routes/api/contacts.js";
-
-// const app = express();
-
-// const formatsLogger = app.get("env") === "development" ? "dev" : "short";
-
-// app.use(logger(formatsLogger));
-// app.use(cors());
-// app.use(express.json());
-
-// app.use("/api/contacts", contactsRouter);
-
-// app.use((req, res) => {
-//   res.status(404).json({ message: "Not found" });
-// });
-
-// app.use((err, req, res, next) => {
-//   res.status(500).json({ message: err.message });
-// });
-
-// export default app;
-
 import express from "express";
 import logger from "morgan";
 import cors from "cors";
 
 import contactsRouter from "./routes/api/contacts.js";
+import { error } from "console";
+// import mongoose from "mongoose";
 
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+// const DB_HOST =
+//   "mongodb+srv://mykytashamota:77443344Yt@cluster0.syw8owd.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+// mongoose
+//   .connect(DB_HOST)
+//   .then(() => console.log("Database connection successful"))
+//   .catch(error.message);
 
 app.use(logger(formatsLogger));
 app.use(cors());
--- server.js
+++ server.js
@@ -1,5 +1,19 @@
-import app from "./app.js";
+import mongoose from "mongoose";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+import app from "./app.js";
+import { error } from "console";
+
+const DB_HOST =
+  "mongodb+srv://mykytashamota:77443344Yt@cluster0.syw8owd.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
