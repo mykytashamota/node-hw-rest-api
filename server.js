61267a29cc36432a8724e85a6384701216951727
mykytashamota <nik.shamota@gmail.com>
2023-12-08 13:01:10 +0200

    add config.js to gitignore

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ import mongoose from "mongoose";
 import app from "./app.js";
 import { error } from "console";
 
-const DB_HOST =
-  "mongodb+srv://mykytashamota:77443344Yt@cluster0.syw8owd.mongodb.net/db-contacts?retryWrites=true&w=majority";
+import DB_HOST from "./config.js";
 
 mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
+    console.log("Database connection successful");
     app.listen(3000);
   })
   .catch((error) => {
